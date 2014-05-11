07be01b2ea0dc1d9d5e29e90500d5be0303a3e81
Jason Wist <jason.wist@gmail.com>
2014-05-10 23:28:46 -0600

    Converting to a class
    Moving things around to become a class.
    
    This will allow me to not have to pass as many parameters.

--- BTParser.py
+++ BTParser.py
@@ -1,57 +1,137 @@
-
-DICT_START = 'd'
-INT_START = 'i'
-LIST_START = 'l'
-STR_START = range(0, 10) # a positive int
-END_CHAR = 'e'
-LENGTH_KEY = ':'
+import string # needed for finding numbers in the string
 
 
+class BTParser(torrent):
+    """
 
-"""
-Read in the file. See wht it gives me.
-"""
+    """
 
-
-data = open("safe", "rb").read()
-
-
-def debencode(data):
-
-    decoded_data = []
-
-    index = 0
-
-    while index in range(len(data)):
-
-        # INT
-        if data[index] == INT_START:
-            print 'int found' # DEBUG
-            int_start = index+1
-
-            while data[index] != END_CHAR:
-                index += 1
-
-            int_end = index
-
-            new_int = data[int_start:int_end]
-
-            decoded_data.append(dict(i=new_int))
+    DICT_START = 'd'
+    INT_START = 'i'
+    LIST_START = 'l'
+    STR_START = string.digits   # So I can compare
+    END_CHAR = 'e'
+    LENGTH_KEY = ':'
 
 
-        # LIST
-        # elif d == LIST_START:
-        #     print 'list found',
-        # elif d == DICT_START:
-        #     print 'dictionary found',
-        # elif d < 0:
-        #     print 'string found',
-        # else:
-        #     'the fuck is that'
+
+
+    def __int__(self, torrent):
+        """
+
+        """
+
+        self.torrent_data = torrent
+
+
+
+
+    def parseTorrent(self):
+        """
+
+        """
+
+        data = self.torrent_data
+
+        parsed_data = {}
+
+        index = 0
+
+        while index in range(len(data)):
+
+            # INT
+            if data[index] == self.INT_START:
+                print 'int found' # DEBUG
+
+                self.decodeInt(index)
+
+            # STRING
+            elif data[index] in self.STR_START:
+                print 'string found' # DEBUG
+
+                self.decodeString(index)
+
+            # LIST
+            # elif data[index] == self.LIST_START:
+            #     print 'list found' # DEBUG
+            #
+            #
+            #     while data[index] != self.END_CHAR:
+
+            # elif d == self.DICT_START:
+            #     print 'dictionary found' # DEBUG
+            #
+            #
+            #     while data[index] != self.END_CHAR:
+
+
+
+
+
+
+            index += 1
+
+        return parsed_data
+
+
+
+    def decodeInt(self, index):
+        """
+
+        """
+        int_start = index+1
+
+        while data[index] != self.END_CHAR:
+            index += 1
+
+        int_end = index
+
+        new_int = data[int_start:int_end]
+
+        decoded_data.append(dict(i=new_int))
+
+
+    def decodeString(self, index):
+        """
+
+        """
+        length_in_str = ''
+        new_str = ''
+
+        while data[index] != LENGTH_KEY:
+            length_in_str += data[index]
+            index += 1
+
+        str_length = int(length_in_str)
 
         index += 1
 
-    print decoded_data
+        while index <= index + str_length:
+            new_str += data[index]
+            index += 1
+
+    def decodeList(self, index):
+        """
+
+        """
+        return True
+
+    def decodeDict(self, index):
+        """
+
+        """
+        return True
 
 
-debencode(data)
+
+if __name__ == '__main__':
+
+    torrent_data = open("safe", "rb").read()
+
+    parser = BTParser(torrent_data)
+
+    parsed_data = parser.parseTorrent()
+
+    print 'Parsed Data:',
+    print parsed_data
+
